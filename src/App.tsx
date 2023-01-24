82d803d72833ca18d1a563bcb0554a10e6c53aa9
bscustomcoding <bscustomcoding@gmail.com>
2023-01-24 12:29:45 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GlobalStyles/Global.css';
+import HomeScreen from './Views/OnBoard/Home/HomeScreen';
+
+function App() {
+  return (
+  <div className="theme font-theme">
+    <HomeScreen />
+  </div> 
+  )
+}
+
+export default App;
