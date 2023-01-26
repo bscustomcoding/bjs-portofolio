793e0576426c0c69553bd8b9544c9eccef58c191
bscustomcoding <bscustomcoding@gmail.com>
2023-01-26 14:43:16 +0100

    added themePalette to table, next adjust darkmode colors

--- src/Views/OnBoard/DataFlow/TimeSheet/TimeSheet.tsx
+++ src/Views/OnBoard/DataFlow/TimeSheet/TimeSheet.tsx
@@ -16,12 +16,17 @@ export default function TimeSheet() {
   const { timeSheets } = React.useContext(DataContext);
   
   return (
-    <Card sx={{maxHeight: '83vh'}}>
+    <Card sx={{maxHeight: '83vh', bgcolor: themePalette.palette.background.primary }}>
       <CardContent >
-    <TableContainer   sx={{ maxHeight: '73vh' }}>
+    <TableContainer   sx={{ maxHeight: '73vh'}}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table" stickyHeader>
-        <TableHead >
-          <TableRow>
+        <TableHead  >
+          <TableRow sx={{
+      "& th": {
+        color: themePalette.palette.text.primary,
+        backgroundColor: themePalette.palette.background.primary
+      }
+    }}>
             <TableCell>Name</TableCell>
             <TableCell align="center">Time</TableCell>
           </TableRow>
@@ -32,17 +37,30 @@ export default function TimeSheet() {
               key={row.name.lastName}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
-              <TableCell component="th" scope="row">
+              <TableCell component="th" scope="row" sx={{
+      "&": {
+        color: themePalette.palette.text.primary
+      }
+    }}>
                 {row.name.lastName} - {row.name.firstName}
               </TableCell>
-              <TableCell align="center">{row.time.hours} - {row.time.min}</TableCell>
+              <TableCell align="center" sx={{
+      "&": {
+        color: themePalette.palette.text.primary
+      }
+    }}>{row.time.hours} - {row.time.min}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
     </TableContainer>
      <TablePagination
-     style={{ borderTop: '1px solid', color: themePalette.palette.divider.primary }}
+     style={{ borderTop: '1px solid' }}
+     sx={{
+      "&": {
+        color: themePalette.palette.text.primary
+      }
+    }}
      rowsPerPageOptions={[5, 10, 25]}
      component="div"
      count={timeSheets.length}
